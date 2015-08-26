4744243b4e4b254a6860c925efedfd11dcc38af5
Dunni Adenuga <ia005@bucknell.edu>
2015-08-25 20:48:49 -0400

    Lab01, Exercise 3, Complete

---
+++ src/lab01/Hello.java
@@ -0,0 +1,26 @@
+/***************************************************************************************************
+ *CSCI205 - Software Engineering and Design
+ *Fall 2015
+ *Instructor: Evan Peck
+ *Section: 8am
+ *
+ *Name: Dunni Adenuga
+ *Date: 25/08/2015
+ *
+ *Lab/Assignment: Lab 01
+ *
+ *Description:"Hello World" program in java
+ *
+ ****************************************************************************************************/
+
+package lab01;
+
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Programming is not a spectator sport!!");
+		System.exit(0);
+	}
+
+}
