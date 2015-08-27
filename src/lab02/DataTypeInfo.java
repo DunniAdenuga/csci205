3e038a9ee2e0ff716fa57b3add87a0550c5b4b75
Dunni Adenuga <ia005@bucknell.edu>
2015-08-27 16:37:05 -0400

    Lab02, Exercise 1, complete

---
+++ src/lab02/DataTypeInfo.java
@@ -0,0 +1,47 @@
+/**
+ * The following is a simple program to illustrate differences between the
+ * primitive types, and use the wrapper classes to gain some information about
+ * those types
+ * 
+ * @author: BRK 2012-fall, updated 2015-spring
+ */
+package lab02;
+
+public class DataTypeInfo 
+{
+
+    public static void main(String[] args) 
+	{
+        byte minByte = Byte.MIN_VALUE, maxByte = Byte.MAX_VALUE;
+        System.out.println("The minimum byte value is " + minByte);
+        System.out.println("The maximum byte value is " + maxByte);
+        System.out.println("The size of byte is " + Byte.SIZE);
+
+        short minShort = Short.MIN_VALUE, maxShort = Short.MAX_VALUE;
+        System.out.println("The minimum short value is " + minShort);
+        System.out.println("The maximum short value is " + maxShort);
+        System.out.println("The size of short is " + Short.SIZE);
+
+	int minInt = Integer.MIN_VALUE, maxInt = Integer.MAX_VALUE;
+        System.out.println("The minimum integer value is " + minInt);
+        System.out.println("The maximum integer value is " + maxInt);
+        System.out.println("The size of integer is " + Integer.SIZE);
+
+	long minLong = Long.MIN_VALUE, maxLong = Long.MAX_VALUE;
+        System.out.println("The minimum long value is " + minLong);
+        System.out.println("The maximum long value is " + maxLong);
+        System.out.println("The size of long is " + Long.SIZE);
+
+	float minFloat = Float.MIN_VALUE, maxFloat = Float.MAX_VALUE;
+        System.out.println("The minimum float value is " + minFloat);
+        System.out.println("The maximum float value is " + maxFloat);
+        System.out.println("The size of float is " + Float.SIZE);
+
+	double minDouble = Double.MIN_VALUE, maxDouble = Double.MAX_VALUE;
+        System.out.println("The minimum double value is " + minDouble);
+        System.out.println("The maximum double value is " + maxDouble);
+        System.out.println("The size of double is " + Double.SIZE);
+
+        // Added the same code for int, long, float and double
+       }
+}
