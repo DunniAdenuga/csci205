6642c7b9c0750fa20fb6ba065001989a91e83e0f
Dunni Adenuga <ia005@bucknell.edu>
2015-09-05 14:47:31 -0400

    Lab04, Exercise 2, complete

---
+++ src/lab04/ArrayChallenge.java
@@ -0,0 +1,97 @@
+/***************************************************************************************************
+ *CSCI205 - Software Engineering and Design
+ *Fall 2015
+ *Instructor: Evan Peck
+ *Section: 8am
+ *
+ *Name: Dunni Adenuga
+ *Date: 05/09/2015
+ *
+ *Lab/Assignment: Lab 04
+ *
+ *Description:Arrays, Switch.
+ *
+ ****************************************************************************************************/
+package lab04;
+import java.util.*;
+
+public class ArrayChallenge 
+{
+public static void main(String[] args) 
+{
+ double[] array1 = { 2, 3, 5, 7, 11 };
+ double[] array2 = { 1, 4, 6, 8, 0 };
+ System.out.printf("array1: %s\n", Arrays.toString(array1));
+ System.out.printf("array2: %s\n", Arrays.toString(array2));
+ System.out.printf("concat test: %s\n",
+Arrays.toString(concat(array1,array2)));
+ System.out.printf("reverse test: %s\n",
+Arrays.toString(reverse(array1)));
+ System.out.printf("altSum test: %.1f\n", altSum(concat(array1,
+array2)));
+ System.out.printf("sorted test 1: %b\n", isSorted(array1));
+ System.out.printf("sorted test 2: %b\n", isSorted(array2));
+}
+
+public static double[] concat(double[] a1, double[] a2)
+{
+double[] answer = new double[a1.length + a2.length];
+int x = 0;
+answer = Arrays.copyOf(a1, a1.length + a2.length);
+for(int i = a1.length; i < a1.length + a2.length; i++)
+	{
+	answer[i] = a2[x];
+	x++;
+	}
+return answer;
+}
+
+static int i =0;
+static int counter = 1;
+static double[] answer;
+public static double[] reverse(double[] a)
+{
+if (counter == 1)
+	answer = new double[a.length]; 
+answer[i] = a[a.length-(i+1)];
+i++;
+counter++;
+if(i < a.length)
+	reverse(a);
+return answer;
+}
+/******************************************************************/
+public static double altSum(double[] a)
+{
+int i=0;
+int counter=1;
+double answer = 0.0;
+while(i < a.length)
+{
+if (counter%2 != 0)
+	answer = answer + a[i];
+else
+	answer = answer - a[i];
+i++;
+counter++;
+}
+return answer;
+}
+/******************************************************************/
+
+public static boolean isSorted(double[] a)
+{
+int i = 0;
+boolean answer = true;
+while((answer == true) && (i < a.length-1))
+{
+if (a[i+1] < a[i])
+	answer = false;
+i++;
+}
+return answer;
+}
+
+}
+
+
