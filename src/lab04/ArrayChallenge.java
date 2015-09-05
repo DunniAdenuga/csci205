32ebbf2b3003cf29fbd9c771625ecd786bbfc0f8
Dunni Adenuga <ia005@bucknell.edu>
2015-09-05 16:41:25 -0400

    Lab04, Exercise 2, complete

--- src/lab04/ArrayChallenge.java
+++ src/lab04/ArrayChallenge.java
@@ -37,6 +37,13 @@ array2)));
 
 public static double[] concat(double[] a1, double[] a2)
 {
+/**
+ *This method should take two arrays of double a1 and a2, and returns a single arr
+ *array that represents a concatenation of both a1 and a2.
+ *
+ *@param a1 & a2 -the 2 arrays to be concatenated
+ *@return a double array containing both arrays
+ */
 double[] answer = new double[a1.length + a2.length];
 int x = 0;
 answer = Arrays.copyOf(a1, a1.length + a2.length);
@@ -53,7 +60,12 @@ static int i =0;
 static int counter = 1;
 static double[] answer;
 public static double[] reverse(double[] a)
-{
+{/**
+   * This method returns the reverse of an array using recursion
+   * 
+   * @param a array to be reversed
+   * @return reverse of <code>s</code>
+   */ 
 if (counter == 1)
 	answer = new double[a.length]; 
 answer[i] = a[a.length-(i+1)];
@@ -66,6 +78,12 @@ return answer;
 /******************************************************************/
 public static double altSum(double[] a)
 {
+/**
+ * This method calculates the alternate sum of elements in a.
+ *
+ *@param a array to be summed
+ *@return returns alternate sum of <code>a</code>
+ */
 int i=0;
 int counter=1;
 double answer = 0.0;
@@ -84,6 +102,12 @@ return answer;
 
 public static boolean isSorted(double[] a)
 {
+/**
+  * This method checks if an array is sorted
+  *
+  * @param a array to be checked
+  * @return true if <code>a</code> is sorted or false if otherwise
+  */
 int i = 0;
 boolean answer = true;
 while((answer == true) && (i < a.length-1))
