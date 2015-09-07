c6777acb28ef9f30e7b18bc7d779474f89395bdd
Dunni Adenuga <ia005@bucknell.edu>
2015-09-07 09:42:14 -0400

    Lab05, Exercise 1, Refactored

--- src/lab05/Hello.java
+++ src/lab05/Hello.java
@@ -33,18 +33,27 @@ public class Hello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello! What is your name ? ");
-        Scanner in = new Scanner(System.in);
-        String name = in.next();
-        System.out.println(name + ", becoming a good programmer takes practice.");
+        greetUser();
 
+        getArrayOfRandomNums();
+    }
+
+    private static void getArrayOfRandomNums() {
         //Some simple code to test the debugger
-        Random rand = new Random();
+
         int[] x = new int[NUM_INTS];
+        Random rand = new Random();
         for (int i = 0; i < NUM_INTS; i++) {
             x[i] = rand.nextInt(100);
         }
         System.out.println(Arrays.toString(x));
+    }
+
+    private static void greetUser() {
+        System.out.print("Hello! What is your name ? ");
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        System.out.println(name + ", becoming a good programmer takes practice.");
     }
 
 }
