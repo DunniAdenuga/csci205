6fb9551ce2d06d70ed926f9da305675231fe0e59
Dunni Adenuga <ia005@bucknell.edu>
2015-09-07 08:59:26 -0400

    Lab05, Exercise 1, a simple hello world program.

---
+++ src/lab05/Hello.java
@@ -0,0 +1,34 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2015
+ *
+ * Name: Dunni Adenuga
+ * Date: Sep 7, 2015
+ * Time: 8:45:09 AM
+ *
+ * Project: csci205
+ * Package: lab05
+ * File: Hello
+ * Description:
+ *
+ * ****************************************
+ */
+package lab05;
+
+/**
+ * A simple program to aid in understanding NetBeans
+ *
+ * @author Dunni Adenuga
+ * @version 0.1
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Becoming a good programmer takes practice.");
+    }
+
+}
