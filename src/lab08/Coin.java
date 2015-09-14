a56353646b11178998c287986285ac637d429cfa
Dunni Adenuga <ia005@bucknell.edu>
2015-09-14 09:23:59 -0400

    "Lab08,  Exercise 1, complete"

---
+++ src/lab08/Coin.java
@@ -0,0 +1,28 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2015
+ *
+ * Name: Dunni Adenuga
+ * Date: Sep 14, 2015
+ * Time: 8:45:00 AM
+ *
+ * Project: csci205
+ * Package: lab08
+ * File: Coin
+ * Description:
+ *
+ * ****************************************
+ */
+package lab08;
+
+/**
+ * Coin - A simple enumerated type to represent the different types of coins
+ *
+ * @author Dunni Adenuga
+ */
+public enum Coin {
+    NICKEL,
+    DIME,
+    QUARTER;
+
+}
