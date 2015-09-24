1ff33b7c959c123330ae9f8edbb119914125f534
Dunni Adenuga <ia005@bucknell.edu>
2015-09-24 17:34:57 -0400

    "Lab10,  Exercise 7, complete"

--- src/lab10/Manager.java
+++ src/lab10/Manager.java
@@ -17,23 +17,51 @@ package lab10;
 
 import java.util.Date;
 
+enum DeptType {
+    ENGINEERING,
+    HR,
+    ADMIN,
+    STAFF,
+    OTHER;
+}
+
 public class Manager extends Employee {
     /**
      * Department Name
      */
-    private String deptName;
+    private DeptType deptName;
 
     public Manager(int empID, String firstName, String lastName, int ssNum,
                    Date hireDate, double salary, String deptName) {
         super(empID, firstName, lastName, ssNum, hireDate, salary);
+        try {
+            this.deptName = DeptType.valueOf(deptName);
+        } catch (IllegalArgumentException e) {
+            this.deptName = DeptType.valueOf("OTHER");//Add to Other Department
+
+        }
+    }
+
+    public Manager(int empID, String firstName, String lastName, int ssNum,
+                   Date hireDate, double salary, DeptType deptName) {
+        super(empID, firstName, lastName, ssNum, hireDate, salary);
+
         this.deptName = deptName;
     }
 
     public String getDeptName() {
+        return deptName.name();
+    }
+
+    public DeptType getDept() {
         return deptName;
     }
 
     public void setDeptName(String deptName) {
+        this.deptName = DeptType.valueOf(deptName);
+    }
+
+    public void setDept(DeptType deptName) {
         this.deptName = deptName;
     }
 
